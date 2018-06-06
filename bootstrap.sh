69a6b763fbfc92223326e12c1a33c2ba86883cf3
James Olds <jamesolds@Jamess-MBP.attlocal.net>
2018-06-06 15:28:42 -0500

    add more apps

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+hash brew 2>/dev/null || { 
 
-brew install ansible
+	echo >&2 "Brew is required but it's not installed. Installing.";
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+}
 
-ansible --version
+hash ansible-playbook 2>/dev/null || { 
+
+	echo >&2 "Ansible is required but it's not installed. Installing.";
+	brew install ansible;
+}
+
+ansible-playbook -i "local," -c local ansible/main.yml
